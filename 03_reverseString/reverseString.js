312019d08750b92b2c7b8b54b36882c2b0ab7edb
Sam <106360016+smccol@users.noreply.github.com>
2022-12-01 11:37:23 -0500

    solve first test by reversing string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    newString = splitString.reverse().join('');
+    return newString;
 };
 
 // Do not edit below this line
