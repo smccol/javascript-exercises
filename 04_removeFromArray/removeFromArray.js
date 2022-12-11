49c8e7c4b7c67671bebfd1f86fa6124a29e2ce64
Sam <106360016+smccol@users.noreply.github.com>
2022-12-10 21:56:48 -0500

    solve 01 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeThese) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < removeThese.length; j++) {
+            if (arr[i] === removeThese[j]) {
+                arr.splice(i, 1);
+                }
+            }        
+    }      
+    return arr;   
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let total = 0;
+    if (firstNumber < 0 || secondNumber < 0) return 'ERROR';
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) return 'ERROR';
+    if (firstNumber < secondNumber) {
+        for (i = firstNumber; i <= secondNumber; i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        for (j = secondNumber; j >= firstNumber; j++) {
+            total += j;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
